e2ed3da2d5fc5b1f270541c05237acbc6ad7c4f1
TheTaterMasher <kalebbenke@gmail.com>
2024-07-09 12:34:05 -0600

    cli is functional with help and exit commands

--- main.go
+++ main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
-	fmt.Println("hello world")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := scanner.Text()
+		if input == "" {
+			continue
+		}
+
+		cliCommands := getCliCommands()
+		command := cliCommands[input]
+
+		if command.name == "" {
+			fmt.Println("invalid command, for a list of commands type help")
+			continue
+		}
+
+		err := command.callback()
+
+		if err != nil {
+			fmt.Println("Exiting Pokedex")
+			break
+		}
+	}
+}
+
+func getCliCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	cliCommands := getCliCommands()
+	fmt.Println("")
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, command := range cliCommands {
+		fmt.Printf("%s: %s \n", command.name, command.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	return errors.New("exiting pokedex")
 }
